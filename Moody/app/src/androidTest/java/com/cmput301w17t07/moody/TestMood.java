7b8e4cef8b8cacf8edb3cb4518b9d203bb116f8f
pang <383066079@qq.com>
2017-02-24 15:48:47 -0700

    new user class

---
+++ Moody/app/src/androidTest/java/com/cmput301w17t07/moody/TestMood.java
@@ -0,0 +1,10 @@
+package com.cmput301w17t07.moody;
+
+/**
+ * Created by Panchy on 2017/2/24.
+ */
+
+public class TestMood {
+
+
+}
--- Moody/app/src/main/java/com/cmput301w17t07/moody/UserAdapter.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/UserAdapter.java
@@ -1,10 +1,31 @@
 package com.cmput301w17t07.moody;
 
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 /**
  * Created by mike on 2017-02-24.
  */
 
-public class UserAdapter{
+public class UserAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
