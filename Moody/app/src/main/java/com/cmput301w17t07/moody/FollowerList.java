065b4af4aa55de7aeadf8db483e14894517aeb12
mike <mike@uws105535usr.uws.ualberta.ca>
2017-02-24 13:45:54 -0700

    Added pending requests stuff to the follower and following list classes

--- Moody/app/src/main/java/com/cmput301w17t07/moody/FollowerList.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/FollowerList.java
@@ -1,9 +1,47 @@
 package com.cmput301w17t07.moody;
 
+import java.util.ArrayList;
+
 /**
  * Created by mike on 2017-02-23.
  */
 
 public class FollowerList {
-    
+    public ArrayList<User> pendingFollowers = new ArrayList<User>();
+    public ArrayList<User> followerList = new ArrayList<User>();
+
+    public void addPending(User user){
+        // When another user requests to follow the user
+        pendingFollowers.add(user);
+    }
+
+    public void deletePending(User user){
+        // When a user rejects another user's follow request
+        pendingFollowers.remove(user);
+    }
+
+    public void countPending(){
+        pendingFollowers.size();
+    }
+
+    public void getPending(int index){
+        pendingFollowers.get(index);
+    }
+
+    public void addFollower(User user){
+        followerList.add(user);
+    }
+
+    public void deleteFollower(User user){
+        followerList.remove(user);
+    }
+
+    public int countFollower(){
+        return followerList.size();
+    }
+
+    public User getFollower(int index){
+        return followerList.get(index);
+    }
+
 }
--- Moody/app/src/main/java/com/cmput301w17t07/moody/FollowingList.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/FollowingList.java
@@ -7,21 +7,40 @@ import java.util.ArrayList;
  */
 
 public class FollowingList {
+    public ArrayList<User> pendingFollowing = new ArrayList<User>();
     public ArrayList<User> followingList = new ArrayList<User>();
 
-    public void addFolloer(User user){
+    public void addPending(User user){
+        // When a user sends a follow request to another user
+        pendingFollowing.add(user);
+    }
+
+    public void deletePending(User user){
+        // When a user's follow request is rejected by another user
+        pendingFollowing.remove(user);
+    }
+
+    public void countPending(){
+        pendingFollowing.size();
+    }
+
+    public void getPending(int index){
+        pendingFollowing.get(index);
+    }
+
+    public void addFollower(User user){
         followingList.add(user);
     }
 
-    public void deleteFolloer(User user){
+    public void deleteFollower(User user){
         followingList.remove(user);
     }
 
-    public int countFolloer(){
+    public int countFollower(){
         return followingList.size();
     }
 
-    public User getFolloer(int index){
+    public User getFollower(int index){
         return followingList.get(index);
     }
 }
