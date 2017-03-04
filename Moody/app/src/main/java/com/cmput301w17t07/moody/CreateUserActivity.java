aef40e2d0c52c883c3d6bfeba3260537ea19722f
Xin <richardhuang1004@gmail.com>
2017-03-04 12:45:52 -0700

    working on login page

--- Moody/app/src/main/java/com/cmput301w17t07/moody/CreateUserActivity.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/CreateUserActivity.java
@@ -1,5 +1,7 @@
 package com.cmput301w17t07.moody;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +9,8 @@ public class CreateUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sp = getSharedPreferences("isFirstIn", Activity.MODE_PRIVATE);
+        boolean isFirstIn = sp.getBoolean("isFirstIn", true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
     }
