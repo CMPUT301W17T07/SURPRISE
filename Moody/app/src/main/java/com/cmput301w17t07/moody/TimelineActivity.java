f01f33e8e27abd65215ff05b90ffa50463a6a71a
Xin <richardhuang1004@gmail.com>
2017-03-04 14:32:54 -0700

    working on temp Internet checker

--- Moody/app/src/main/java/com/cmput301w17t07/moody/TimelineActivity.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/TimelineActivity.java
@@ -1,7 +1,11 @@
 package com.cmput301w17t07.moody;
 
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,28 +13,43 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class TimelineActivity extends AppCompatActivity {
+    ConnectivityManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences sp = getSharedPreferences("isFirstIn", Activity.MODE_PRIVATE);
         boolean isFirstIn = sp.getBoolean("isFirstIn", true);
-        if(isFirstIn) {
+        if (isFirstIn) {
             sp.edit().putBoolean("isFirstIn", false).commit();
             setContentView(R.layout.activity_create_user);
             Toast.makeText(TimelineActivity.this, " Welcome to Moody! ", Toast.LENGTH_SHORT).show();
             Button registerButton = (Button) findViewById(R.id.register);
             registerButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
-                    Toast.makeText(TimelineActivity.this, "Registtration successful!", Toast.LENGTH_SHORT).show();
-                    setContentView(R.layout.activity_timeline);
+                    if(checkNetworkState() == false) {
+                        Toast.makeText(TimelineActivity.this, "Internet unavailable! \n " +
+                                "Please check Internet", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(TimelineActivity.this, "Registtration successful!", Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.activity_timeline);
+                    }
                 }
             });
-        }
-
-        else{
+        } else {
             setContentView(R.layout.activity_timeline);
         }
+    }
 
+    //Internet checker temp, maybe need change in the future
+    private boolean checkNetworkState() {
+        manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        if (info == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
