94ef3ecb44e662f4ad67d8a139c1fd4d6f492ecf
mikeualberta <simion@ualberta.ca>
2017-03-05 15:42:24 -0700

    added ElasticMoodCont

--- Moody/app/src/main/java/com/cmput301w17t07/moody/ElasticMoodController.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/ElasticMoodController.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 
 import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
 /**
  * Created by mike on 2017-03-05.
@@ -46,9 +48,47 @@ public class ElasticMoodController extends ElasticController {
 
     public class GetMoods extends AsyncTask<String, Void, ArrayList<Mood>> {
         @Override
-        protected ArrayList<Mood> doInBackground(String... params) {
+        protected ArrayList<Mood> doInBackground(String... search_parameters) {
             verifySettings();
-            return null;
+
+            ArrayList<Mood> moods = new ArrayList<Mood>();
+
+            String query;
+            //search param 0 = feeling, 1 = date, 2 = message (FOR NOW)
+            if ((search_parameters[0]=="") && (search_parameters[1]=="") && search_parameters[2]==""){
+                query="{\"from\":0,\"size\":100}"; // CHANGE SIZE and NOT sure if this is what we will want
+            }else {
+                query = "{\n" +
+                        "    \"query\" : {\n" +
+                        "        \"term\" : { \"message\" :\"" + search_parameters[0] + "\" }\n" +
+                        "    }\n" +
+                        "}";
+                System.out.println("this is query" + query);
+            }
+
+            // TODO Build the query
+            Search search = new Search.Builder(query)
+                    .addIndex("testing")
+                    .addType("tweet").build();
+
+            try {
+                // TODO get the results of the query
+
+
+                SearchResult result = client.execute(search);
+                if(result.isSucceeded()){
+                    List<NormalTweet> foundTweets = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(foundTweets);
+                }
+                else {
+                    Log.i("Error", "The search query failed to find any tweets that matched, buddy");
+                }
+            }
+            catch (Exception e) {
+                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+            }
+
+            return tweets;
         }
     }
 
