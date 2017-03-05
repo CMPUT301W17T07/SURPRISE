aef1f46a2eb51c2b9bc2309317d1d73a90795d91
Xin <richardhuang1004@gmail.com>
2017-03-04 17:34:27 -0700

    add network checker

--- Moody/app/src/main/java/com/cmput301w17t07/moody/TimelineActivity.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/TimelineActivity.java
@@ -1,17 +1,19 @@
 package com.cmput301w17t07.moody;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class TimelineActivity extends AppCompatActivity {
-
+    ConnectivityManager manager;
     private EditText usernameText;
 
     @Override
@@ -31,14 +33,20 @@ public class TimelineActivity extends AppCompatActivity {
              */
             registerButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
-                    String username = usernameText.getText().toString();
-                    User newUser = new User(username);
-                    // Want to add something here like user.checkUsername that will check the database to see if username
-                    // is unique. if(user.checkUsername == false) {return} else ....
-                    ElasticSearchMoodyController.AddUser addUser = new ElasticSearchMoodyController.AddUser();
-                    addUser.execute(newUser);
-                    Toast.makeText(TimelineActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
-                    setContentView(R.layout.activity_timeline);
+                    if (checkNetworkState() == false) {
+                        Toast.makeText(TimelineActivity.this, "Internet not available \n" +
+                                "Plaese check internet", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        String username = usernameText.getText().toString();
+                        User newUser = new User(username);
+                        // Want to add something here like user.checkUsername that will check the database to see if username
+                        // is unique. if(user.checkUsername == false) {return} else ....
+                        ElasticSearchMoodyController.AddUser addUser = new ElasticSearchMoodyController.AddUser();
+                        addUser.execute(newUser);
+                        Toast.makeText(TimelineActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.activity_timeline);
+                    }
                 }
             });
         }
@@ -47,5 +55,16 @@ public class TimelineActivity extends AppCompatActivity {
             setContentView(R.layout.activity_timeline);
         }
 
+    }
+    //Internet checker temp, maybe need change later
+    private boolean checkNetworkState() {
+        manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        if (info == null) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
