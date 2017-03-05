53fb075146ebdb96b14c1e10d3de51ad44c69233
Xin <richardhuang1004@gmail.com>
2017-03-04 17:22:37 -0700

    swith

--- Moody/app/src/main/java/com/cmput301w17t07/moody/ElasticController.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/ElasticController.java
@@ -16,7 +16,7 @@ public class ElasticController {
     protected static void verifySettings() {
         if (client == null) {
             DroidClientConfig.Builder builder =
-                    new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080/");
+                    new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
             DroidClientConfig config = builder.build();
 
             JestClientFactory factory = new JestClientFactory();
--- Moody/app/src/main/java/com/cmput301w17t07/moody/ElasticSearchMoodyController.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/ElasticSearchMoodyController.java
@@ -1,6 +1,5 @@
 package com.cmput301w17t07.moody;
 
-
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -13,16 +12,14 @@ import io.searchbox.core.Index;
  * Created by mike on 2017-02-23.
  */
 
-public class ElasticSearchMoodyController extends ElasticController {
-
-
+public class ElasticSearchMoodyController extends ElasticController{
     public static class AddUser extends AsyncTask<User, Void, Void> {
         @Override
         protected Void doInBackground(User... users) {
             verifySettings();
 
             for (User user: users) {
-                Index index = new Index.Builder(user).index("cmput301w17t07").type("user").build();
+                Index index = new Index.Builder(user).index("cmput301w17t07").type("user").id("12321").build();
 
 
                 try {
@@ -37,9 +34,6 @@ public class ElasticSearchMoodyController extends ElasticController {
                     Log.i("Error", "The application failed to add the user");
                 }
             }
-
-
-
             return null;
         }
     }
@@ -51,5 +45,7 @@ public class ElasticSearchMoodyController extends ElasticController {
             return null;
         }
     }
+
+
 
  }
