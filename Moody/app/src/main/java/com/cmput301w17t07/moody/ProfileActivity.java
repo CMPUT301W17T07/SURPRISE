c34a8a817ddda56eeaf3a3a490188b3ab1de65f5
mikeualberta <simion@ualberta.ca>
2017-03-08 15:07:03 -0700

    working on mood timeline adapter

--- Moody/app/src/main/java/com/cmput301w17t07/moody/ProfileActivity.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/ProfileActivity.java
@@ -1,13 +1,66 @@
 package com.cmput301w17t07.moody;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.id.list;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ListView oldMoodList;
+    private hopefuladapter adapter;
+    private ArrayList<Mood> moodArrayList = new ArrayList<Mood>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
+
+        @Override
+        protected void onStart(){
+            super.onStart();
+            ElasticMoodController.GetUserMoods getUserMoods = new ElasticMoodController.GetUserMoods();
+            getUserMoods.execute("xin");
+
+            try {
+                moodArrayList= getUserMoods.get();
+                System.out.println("this is moodlist"+moodArrayList);
+
+            }catch (Exception e){
+                Log.i("error","failed to get the mood out of the async matched");
+            }
+            adapter = new hopefuladapter(this, moodArrayList);
+//                @Override
+//                public View getView(int position, View convertView, ViewGroup parent) {
+//                    View view = super.getView(position, convertView, parent);
+//                    TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+//                    TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+//                /* Setting the text that will appear on the two lines of the list entry */
+//                    text1.setText(moodArrayList.get(position).getUsername());
+//                /* Bolding the name entry */
+//                    text1.setTypeface(null, Typeface.BOLD);
+//
+//                    text2.setText("Feeling: " + moodArrayList.get(position).getFeeling() );
+//                    return view;
+//                }
+
+
+
+            oldMoodList.setAdapter(adapter);
+            }
 }
+
+
---
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/TimelineAdapter.java
@@ -0,0 +1,50 @@
+package com.cmput301w17t07.moody;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mike on 2017-03-08.
+ */
+
+public class TimelineAdapter extends ArrayAdapter<Mood> {
+    ArrayList<Mood> moodTimeline;
+    ArrayList<Mood> filteredMoodTimeline; //won't be implemented for now
+    Context context;
+    int layout_timeline_list;
+
+    /**
+     * Used https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+     * as a reference
+     * @param context
+     * @param layout_timeline_list
+     * @param moodTimeline
+     */
+    public TimelineAdapter(Context context, int layout_timeline_list, ArrayList<Mood> moodTimeline){
+        super(context, layout_timeline_list, moodTimeline);
+        this.filteredMoodTimeline = moodTimeline; //current 'filter' is whatever is passed in.
+        this.context = context;
+        this.layout_timeline_list = layout_timeline_list;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+
+
+        // Do I need to check the view?
+
+        Mood mood = getItem(position); //do I need to override the getView position?
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.timeline_list, parent, false);
+        }
+    }
+
+
+}
