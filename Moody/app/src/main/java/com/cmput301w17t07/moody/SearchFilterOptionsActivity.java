311a4c029286b8a8a49aaf3d6c25c3d6d1d4642d
pang <383066079@qq.com>
2017-03-11 12:25:36 -0700

    change search

--- Moody/app/src/main/java/com/cmput301w17t07/moody/SearchFilterOptionsActivity.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/SearchFilterOptionsActivity.java
@@ -4,14 +4,44 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class SearchFilterOptionsActivity extends BarMenuActivity {
+
+    private UserAdapter adapter;
+    private List<User> userList;
+    private ListView olduserlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_filter_options);
         setUpMenuBar(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        ElasticSearchUserController.GetUser getUser=new ElasticSearchUserController.GetUser();
+        getUser.execute("");
+
+        ListView olduserlist=(ListView) findViewById(R.id.listOfSearch);
+        try{
+            userList=getUser.get();
+            System.out.println("this is userlist"+
+                    userList);
+        }catch (Exception e){
+            Log.i("error","failed to get the user out of the async matched");
+        }
+
+        adapter=new UserAdapter(getApplicationContext(),userList);
+        olduserlist.setAdapter(adapter);
+
+
+
     }
 }
--- Moody/app/src/main/java/com/cmput301w17t07/moody/SearchUserActivity.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/SearchUserActivity.java
@@ -2,6 +2,8 @@ package com.cmput301w17t07.moody;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
 import java.util.List;
 
@@ -9,6 +11,8 @@ public class SearchUserActivity extends AppCompatActivity {
 
     private UserAdapter adapter;
     private List<User> userList;
+    private ListView olduserlist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,22 @@ public class SearchUserActivity extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
+        ElasticSearchUserController.GetUser getUser=new ElasticSearchUserController.GetUser();
+        getUser.execute("");
+
+        olduserlist=(ListView) findViewById(R.id.listOfSearch);
+        try{
+            userList=getUser.get();
+            System.out.println("this is userlist"+
+                    userList);
+        }catch (Exception e){
+            Log.i("error","failed to get the user out of the async matched");
+        }
+
+        adapter=new UserAdapter(this,userList);
+        olduserlist.setAdapter(adapter);
+
+
 
     }
 }
