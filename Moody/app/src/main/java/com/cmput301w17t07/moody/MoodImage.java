b4ac6b6e9078a212abba3b5b31448b128ecaf550
mikeualberta <simion@ualberta.ca>
2017-03-13 11:38:50 -0600

    javadocs for moodImage class

--- Moody/app/src/main/java/com/cmput301w17t07/moody/MoodImage.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/MoodImage.java
@@ -13,6 +13,11 @@ import io.searchbox.annotations.JestId;
  * Created by mike on 2017-03-11.
  */
 
+/**
+ * The MoodImage model class for the Moody application. The MoodImage is a class object used to
+ * store user's mood image data efficiently on the database.
+ */
+
 public class MoodImage {
 
     private String encodedImage;
@@ -32,6 +37,11 @@ public class MoodImage {
         this.moodID = id;
     }
 
+    /**
+     * This method decodes the encodedImage string that is stored on the Moody application's server
+     * and allows the image to be displayed from the server in the ViewMoodActivity.
+     * @return bitmap       The decoded bitmap image from the server
+     */
     public Bitmap decodeImage() {
         Bitmap bitmap;
         bitmap = null;
@@ -44,6 +54,23 @@ public class MoodImage {
     }
 
 
+
+    /**
+     * This method encodes and compresses a user's supplied bitmap image for appropriate storage
+     * on the application's server. Images stored on the server are all under 65536 bytes.
+     *
+     * Logic for compression of the image is from:
+     * link: http://blog.csdn.net/harryweasley/article/details/51955467
+     * Author: Harry Weasley
+     * Taken by: Xin Huang 2017/03/11
+     *
+     * Logic for encoding of image string was based upon:
+     * link: http://stackoverflow.com/questions/12796579/how-to-send-image-bitmap-to-server-in-android-with-multipart-form-data-json
+     * Author: Carnal
+     * Taken by: Michael Simion 2017/03/10
+     * @param moodImage         The user's supplied image for his or her mood
+     * @return encodedImage     The encodedImage string that will be stored within the object on the server
+     */
     public String encodeImage(Bitmap moodImage){
         if(moodImage == null){
             return null;
@@ -51,7 +78,7 @@ public class MoodImage {
 
         System.out.println("Image size is too big! " + ((moodImage.getRowBytes() * moodImage.getHeight()) / 8));
         try {
-            //compress taken from http://blog.csdn.net/harryweasley/article/details/51955467
+            // Compression of image. From: http://blog.csdn.net/harryweasley/article/details/51955467
             // for compressing the image to meet the project storage requirements
             while (((moodImage.getRowBytes() * moodImage.getHeight()) / 8) > 65536) {
                 System.out.println("Image size is too big! " + ((moodImage.getRowBytes() * moodImage.getHeight()) / 8));
@@ -70,8 +97,7 @@ public class MoodImage {
         } catch (Exception E){
 
         }
-
-        // http://stackoverflow.com/questions/12796579/how-to-send-image-bitmap-to-server-in-android-with-multipart-form-data-json
+        // Encoding of Image
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         moodImage.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
         byte[] imageBytes = outputStream.toByteArray();
