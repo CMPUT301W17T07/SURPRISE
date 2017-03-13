d83a94f84b1d1ed492a0c3a94acaa4373bdee312
mikeualberta <simion@ualberta.ca>
2017-03-13 11:11:53 -0600

    Removed some unused functions from Mood class

--- Moody/app/src/main/java/com/cmput301w17t07/moody/Mood.java
+++ Moody/app/src/main/java/com/cmput301w17t07/moody/Mood.java
@@ -36,7 +36,6 @@ public class Mood implements Serializable {
     private String displayUsername;
 
     private String encodedImage;
-//    private Image emoji;
 
     @JestId
 
@@ -50,20 +49,7 @@ public class Mood implements Serializable {
         this.id = id;
     }
 
-//    public enum Feeling{
-//        anger, confusion, disgust, fear, happy,sad, shame, surprise
-//    }
-//
-//    public Mood(String feeling, String username){
-//        this.feeling = feeling;
-//        this.username = username;
-//        this.moodMessage =null;
-//        this.date=new Date();
-//        this.location=null;
-//        this.moodImage=null;
-//        this.socialSituation=null;
-//
-//    }
+
 
     public Mood(String feeling, String username, String moodMessage, Location location,
                 String imageID, String socialSituation){
@@ -108,16 +94,17 @@ public class Mood implements Serializable {
         this.location = location;
     }
 
-    public Bitmap getMoodImage() {
-        Bitmap bitmap;
-        bitmap = null;
-        if(encodedImage != null){
-            byte[]bitmapArray = null;
-            bitmapArray= Base64.decode(encodedImage, Base64.URL_SAFE);
-            bitmap= BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
-        }
-        return bitmap;
-    }
+    // todo might be utilized for offline functionality
+//    public Bitmap getMoodImage() {
+//        Bitmap bitmap;
+//        bitmap = null;
+//        if(encodedImage != null){
+//            byte[]bitmapArray = null;
+//            bitmapArray= Base64.decode(encodedImage, Base64.URL_SAFE);
+//            bitmap= BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
+//        }
+//        return bitmap;
+//    }
 
     public String getSocialSituation() {
         return socialSituation;
@@ -131,40 +118,7 @@ public class Mood implements Serializable {
         this.moodImageID = moodImage;
     }
 
-//    public String encodeImage(Bitmap moodImage){
-//        if(moodImage == null){
-//            return null;
-//        }
-//
-//        try {
-//            //compress taken from http://blog.csdn.net/harryweasley/article/details/51955467
-//            // for compressing the image to meet the project storage requirements
-//            while (((moodImage.getRowBytes() * moodImage.getHeight()) / 8) > 65536) {
-//                System.out.println("Image size is too big! " + ((moodImage.getRowBytes() * moodImage.getHeight()) / 8));
-//
-//                BitmapFactory.Options options2 = new BitmapFactory.Options();
-//                options2.inPreferredConfig = Bitmap.Config.RGB_565;
-//
-//                Matrix matrix = new Matrix();
-//                matrix.setScale(0.5f, 0.5f);
-//                moodImage = Bitmap.createBitmap(moodImage, 0, 0, moodImage.getWidth(),
-//                        moodImage.getHeight(), matrix, true);
-//
-//                System.out.println("Image size is too big! " + ((moodImage.getRowBytes() * moodImage.getHeight()) / 8));
-//
-//            }
-//        } catch (Exception E){
-//
-//        }
-//        System.out.println("Image size is too big! " + ((moodImage.getRowBytes() * moodImage.getHeight()) / 8));
-//        // http://stackoverflow.com/questions/12796579/how-to-send-image-bitmap-to-server-in-android-with-multipart-form-data-json
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//        moodImage.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-//        byte[] imageBytes = outputStream.toByteArray();
-//        encodedImage = Base64.encodeToString(imageBytes,Base64.URL_SAFE);
-//        return encodedImage;
-//
-//    }
+
 
     public String getFeeling() {
         return feeling;
@@ -182,15 +136,6 @@ public class Mood implements Serializable {
         this.username = username;
     }
 
-//    public void setEmoji(){
-//        switch (this.getFeeling()) {
-//            case "happy":
-//                 //todo set this.emoji to be equal to image
-//                break;
-//            case "sad":
-//                break;
-//        }
-//    }
 
 
     public String getDisplayUsername() {
