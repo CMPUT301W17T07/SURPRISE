47c9951f7a1e77ab5c7b1c800e0729bbe1c2abd1
mikeualberta <simion@ualberta.ca>
2017-04-02 21:02:04 -0600

    updated model test cases

--- Moody/app/src/androidTest/java/com/cmput301w17t07/moody/MoodListTest.java
+++ Moody/app/src/androidTest/java/com/cmput301w17t07/moody/MoodListTest.java
@@ -18,69 +18,111 @@ package com.cmput301w17t07.moody;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by Panchy on 2017/2/24.
  */
 
 
-//public class MoodListTest extends ActivityInstrumentationTestCase2 {
-//    public MoodListTest() {
-//        super(MoodList.class);
-//    }
-//
-//    @Test
-//    public void testAddMood() {
-//        MoodList moodList=new MoodList();
-//        Mood mood=new Mood("happy","xin");
-//        moodList.addMood(mood);
-//        assertTrue(moodList.hasMood(mood));
-//        // testing to see if adding the same mood object will result in an error
-//        try{
-//            moodList.addMood(mood);
-//            assertTrue(false);
-//        } catch (IllegalArgumentException e){
-//            assertTrue(true);
-//        }
-//    }
-//
-//    @Test
-//    public void testHasMood(){
-//        // TDD: Test > Fail > Code > Pass (LOOP)
-//        MoodList moodList = new MoodList();
-//        Mood mood = new Mood("test mood","xin");
-//        // first checking that list doesn't have tweet before its addition
-//        assertFalse(moodList.hasMood(mood));
-//        moodList.addMood(mood);
-//        assertTrue(moodList.hasMood(mood));
-//    }
-//
-//    @Test
-//    public void testDeleteMood(){
-//        MoodList moodList=new MoodList();
-//        Mood mood=new Mood("happy","xin");
-//        moodList.addMood(mood);
-//
-//        moodList.deleteMood(mood);
-//        assertEquals(moodList.countMoodList(),0);
-//
-//    }
-//
-//    @Test
-//    public void testCountMoodList(){
-//        MoodList moodList=new MoodList();
-//        Mood mood=new Mood("happy","xin");
-//        assertEquals(moodList.countMoodList(),0);
-//        moodList.addMood(mood);
-//        assertEquals(moodList.countMoodList(),1);
-//    }
-//
-//    @Test
-//    public void testGetMood(){
-//        MoodList moodList=new MoodList();
-//        Mood mood=new Mood("happy","xin");
-//        moodList.addMood(mood);
-//
-//        assertEquals(mood,moodList.getMood(0));
-//    }
-//}
+public class MoodListTest extends ActivityInstrumentationTestCase2 {
+    public MoodListTest() {
+        super(MoodList.class);
+    }
+
+    @Test
+    public void testAddMood(){
+        MoodList moodList = new MoodList();
+        Mood mood = new Mood("anger", "testing", "", 0.0, 0.0, null, "", new Date(), "");
+
+        moodList.addMood(mood);
+
+        // determine that offline added count is 1
+        assertTrue(moodList.addedOffline.size() == 1);
+        // determine that moodlist size count is 1
+        assertTrue(moodList.countMoodList() == 1);
+    }
+
+    @Test
+    public void testdeleteMood(){
+        MoodList moodList = new MoodList();
+        Mood mood = new Mood("anger", "testing", "", 0.0, 0.0, null, "", new Date(), "");
+        Mood mood1 = new Mood("anger", "testing", "", 0.0, 0.0, null, "", new Date(), "");
+
+        mood.setId(UUID.randomUUID().toString());
+        mood1.setId(UUID.randomUUID().toString());
+
+
+        moodList.addMood(mood);
+        moodList.addMood(mood1);
+
+        // determine that offline added count is 2
+        assertTrue(moodList.addedOffline.size() == 2);
+        // determine that moodlist size count is 2
+        assertTrue(moodList.countMoodList() == 2);
+
+        moodList.deleteMood(mood);
+        // check the moodlist size
+        assertTrue(moodList.countMoodList() == 1);
+        // check the deleted offline moodlist size
+        assertTrue(moodList.deletedOffline.size() == 1);
+        // check that it contains the proper mood
+        assertTrue(moodList.deletedOffline.contains(mood));
+        // make sure moodlist deleted the proper entry
+        assertTrue(moodList.getMoods().contains(mood1));
+
+
+    }
+
+    @Test
+    public void testGetMood(){
+        MoodList moodList=new MoodList();
+        Mood mood = new Mood("anger", "testing", "", 0.0, 0.0, null, "", new Date(), "");
+        moodList.addMood(mood);
+
+        assertEquals(mood,moodList.getMood(0));
+    }
+
+    @Test
+    public void testCountMoodList(){
+        MoodList moodList=new MoodList();
+        Mood mood = new Mood("anger", "testing", "", 0.0, 0.0, null, "", new Date(), "");
+        assertEquals(moodList.countMoodList(),0);
+        moodList.addMood(mood);
+        assertEquals(moodList.countMoodList(),1);
+    }
+
+    @Test
+    public void testEditMood(){
+
+        MoodList moodList = new MoodList();
+        Mood mood = new Mood("anger", "testing", "", 0.0, 0.0, null, "", new Date(), "");
+
+        moodList.addMood(mood);
+
+        // determine that offline added count is 1
+        assertTrue(moodList.addedOffline.size() == 1);
+        // determine that moodlist size count is 1
+        assertTrue(moodList.countMoodList() == 1);
+
+
+        Mood editMood = new Mood("anger", "edit", "", 0.0, 0.0, null, "", new Date(), "");
+
+        mood.setId(UUID.randomUUID().toString());
+        editMood.setId(mood.getId());
+
+
+        moodList.editMood(editMood, mood);
+
+
+        // Ensure that the added offline list was incremented
+        assertTrue(moodList.addedOffline.size() == 1);
+        // ensure that moodlist contains the edited mood
+        assertTrue(moodList.getMood(0).getMoodMessage().equals("edit"));
+
+    }
+}
 
